6b8065b36e5ba626485bbd2289f9d78c6b373047
erurang <erurang@naver.com>
2020-12-05 05:42:01 +0900

    ver 0.1

---
+++ src/clock.js
@@ -0,0 +1,24 @@
+const clock = document.querySelector(".clock");
+
+function handleTime() {
+  const timeNow = new Date();
+  const year = timeNow.getFullYear();
+  const month = timeNow.getMonth();
+  const date = timeNow.getDate();
+  const hours = timeNow.getHours();
+  const minutes = timeNow.getMinutes();
+  const seconds = timeNow.getSeconds();
+
+  clock.innerText = `${year}년 ${month}월 ${
+    date < 10 ? `0${date}일` : `${date}일`
+  }  ${hours < 10 ? `0${hours}` : `${hours}`}:${
+    minutes < 10 ? `0${minutes}` : `${minutes}`
+  }:${seconds < 10 ? `0${seconds}` : `${seconds}`}
+  `;
+}
+
+function init() {
+  setInterval(handleTime, 1000);
+}
+
+init();
